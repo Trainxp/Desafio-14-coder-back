17ae0f8909e3387a200959ab1c849374298b4a30
Nahuel Enriquez <61632891+Trainxp@users.noreply.github.com>
2023-01-19 17:57:25 -0300

    cambios

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const Container = require('./container.js');
 require('./middlewares/auth');
 const { optionsMariaDB, optionsSQLite3 } = require('./options/config.js');
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const app = express();
 // const PORT =  8050;
 
